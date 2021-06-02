0b296df1888c98e1a0dd470d8851fdd57afab40f
Kushlendrasingh <kushlendrasingh02@gmail.com>
2021-06-02 09:26:28 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor modi molestiae odit quos necessitatibus, facilis cumque ut eius, laudantium aspernatur magni similique vel cupiditate eum a, excepturi debitis dignissimos quo. Culpa quisquam beatae praesentium?</p>
+        </div>
+    )
+}
